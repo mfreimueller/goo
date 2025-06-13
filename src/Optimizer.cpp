6739fc227350a4df7f6f0f40628fa00e317d9fb0
Michael Freimüller <michael@mfreimueller.com>
2025-06-13 19:37:49 +0200

    refactor Optimizer to use pass-system, plus implement Group-, Reset- and TransferPass

--- src/Optimizer.cpp
+++ src/Optimizer.cpp
@@ -4,31 +4,45 @@
 
 #include "Optimizer.h"
 
+typedef std::vector<goo::OptimizationPass *> PassVector;
+
 namespace goo {
     std::shared_ptr<Payload> Optimizer::run(const std::shared_ptr<Payload> payload) {
-        // reset state of optimizer to allow for reuse
-        optimizedStmts.clear();
+        // Add new passes to this list in the required order.
+        const PassVector passes = {
+            new GroupPass,
+            new ResetPass,
+            new TransferPass
+        };
 
         const auto stmtPayload = std::static_pointer_cast<StmtPayload>(payload);
 
-        const auto stmts = stmtPayload->stmts;
-        const auto optimizedStmts = optimizeStmts(stmts);
+        auto stmts = stmtPayload->stmts;
 
-        return std::make_shared<StmtPayload>(StmtPayload { .stmts = optimizedStmts });
+        for (const auto &it: passes) {
+            stmts = it->run(stmts);
+            delete it;
+        }
+
+        return std::make_shared<StmtPayload>(StmtPayload{.stmts = stmts});
     }
 
-    std::vector<std::shared_ptr<Stmt>> Optimizer::optimizeStmts(const std::vector<std::shared_ptr<Stmt>> &stmts) {
-        std::vector<std::shared_ptr<Stmt>> optimizedStmts;
+    //
+    // GroupPass
+    //
+
+    StmtVector GroupPass::run(const StmtVector &stmts) const { // NOLINT(*-no-recursion)
+        StmtVector optimizedStmts;
 
         for (int idx = 0; idx < stmts.size(); idx++) {
-            if (const auto& stmt = stmts[idx]; stmt->type < OUT) {
-                auto grouped = groupStmts(idx, stmts);
-                optimizedStmts.insert(optimizedStmts.end(), grouped.begin(), grouped.end());
+            if (const auto &stmt = stmts[idx]; stmt->type < OUT) {
+                auto groupedStmt = groupStmts(idx, stmts);
+                optimizedStmts.emplace_back(groupedStmt);
             } else if (stmt->type == IF) {
                 const auto conditional = std::static_pointer_cast<Conditional>(stmt);
-                auto newStmts = optimizeStmts(conditional->stmts);
+                auto subStmts = run(conditional->stmts);
 
-                optimizedStmts.emplace_back(new Conditional(stmt->column, stmt->line, newStmts));
+                optimizedStmts.emplace_back(new Conditional(stmt->column, stmt->line, subStmts));
             } else {
                 optimizedStmts.push_back(stmt);
             }
@@ -37,9 +51,7 @@ namespace goo {
         return optimizedStmts;
     }
 
-    std::vector<std::shared_ptr<Stmt>> Optimizer::groupStmts(int &idx, const std::vector<std::shared_ptr<Stmt> > &stmts) {
-        std::vector<std::shared_ptr<Stmt>> optimizedStmts;
-
+    std::shared_ptr<Stmt> GroupPass::groupStmts(int &idx, const StmtVector &stmts) const {
         auto stmt = stmts[idx];
 
         const int line = stmt->line;
@@ -73,19 +85,128 @@ namespace goo {
 
         if (moves > 0) {
             if (isByteOp) {
-                optimizedStmts.push_back(std::make_shared<IncrementByte>(line, column, moves));
+                return std::make_shared<IncrementByte>(column, line, moves);
             } else {
-                optimizedStmts.push_back(std::make_shared<IncrementPtr>(line, column, moves));
+                return std::make_shared<IncrementPtr>(column, line, moves);
             }
         } else if (moves < 0) {
             // As we have negative moves, we must negate them to have the positive value
             moves = -moves;
 
             if (isByteOp) {
-                optimizedStmts.push_back(std::make_shared<DecrementByte>(line, column, moves));
+                return std::make_shared<DecrementByte>(column, line, moves);
             } else {
-                optimizedStmts.push_back(std::make_shared<DecrementPtr>(line, column, moves));
+                return std::make_shared<DecrementPtr>(column, line, moves);
             }
+        }
+
+        return stmt;
+    }
+
+    //
+    // ResetPass
+    //
+
+    StmtVector ResetPass::run(const StmtVector &stmts) const {
+        StmtVector optimizedStmts;
+
+        // We iterate over all statements trying to detect a pattern of Conditional->Decrease. In this case we
+        // insert a new reset statement in place of the conditional.
+        for (int idx = 0; idx < stmts.size(); idx++) {
+            const auto &stmt = stmts[idx];
+            if (stmt->matches({IF, DEC_BYTE, FI})) {
+                // Now that we know that we reset the current byte we also check for an assign statement directly
+                // afterward: [-]+
+                int initialValue = 0;
+
+                if (idx + 1 < stmts.size() && stmts[idx + 1]->type == INC_BYTE) {
+                    const auto incByte = std::static_pointer_cast<IncrementByte>(stmts[idx + 1]);
+                    initialValue = incByte->count;
+
+                    // increase index by one to skip this increase.
+                    idx++;
+                }
+
+                optimizedStmts.emplace_back(new Reset(stmt->column, stmt->line, initialValue, 0));
+                continue;
+            }
+
+            // If we cannot detect a reset statement, we simply add it untouched.
+            optimizedStmts.push_back(stmt);
+        }
+
+        return optimizedStmts;
+    }
+
+    //
+    // TransferPass
+    //
+
+    StmtVector TransferPass::run(const StmtVector &stmts) const {
+        StmtVector optimizedStmts;
+
+        // We iterate over all statements trying to detect a pattern of Conditional->Decrease. In this case we
+        // insert a new reset statement in place of the conditional.
+        for (const auto &stmt: stmts) {
+            // Matched patterns: [>+<-], [->+<], [<+>-] or [>-<+>]
+            if (stmt->matches({IF, DEC_BYTE, INC_PTR, INC_BYTE, DEC_PTR, FI}) ||
+                stmt->matches({IF, INC_PTR, INC_BYTE, DEC_PTR, DEC_BYTE, FI}) ||
+                stmt->matches({IF, DEC_PTR, INC_BYTE, INC_PTR, DEC_BYTE, FI}) ||
+                stmt->matches({IF, INC_PTR, DEC_BYTE, DEC_PTR, INC_BYTE, INC_PTR, FI})) { // TODO: test this last combination
+                const auto conditional = std::static_pointer_cast<Conditional>(stmt);
+
+                int offset = 0;
+                int addOffset = 0;
+                int subOffset = 0;
+
+                // We are only interested in direct copies. No [>++<-]. This is matter of a different optimization.
+                // If we detect that we are or remove more than one bit at a time, we bail out.
+                bool isValidCopy = true;
+
+                // we track all ptr operations to calculate the offset, which we then use to write the correct copy
+                // operation
+                for (const auto &it: conditional->stmts) {
+                    if (it->type == INC_PTR) {
+                        const auto incPtr = std::static_pointer_cast<IncrementPtr>(it);
+                        offset += incPtr->count;
+                    } else if (it->type == DEC_PTR) {
+                        const auto decPtr = std::static_pointer_cast<DecrementPtr>(it);
+                        offset -= decPtr->count;
+                    } else if (it->type == INC_BYTE) {
+                        const auto incByte = std::static_pointer_cast<IncrementByte>(it);
+                        if (incByte->count > 1) {
+                            isValidCopy = false;
+                            break;
+                        }
+
+                        addOffset = offset;
+                    } else if (it->type == DEC_BYTE) {
+                        const auto decByte = std::static_pointer_cast<DecrementByte>(it);
+                        if (decByte->count > 1) {
+                            isValidCopy = false;
+                            break;
+                        }
+
+                        subOffset = offset;
+                    }
+                }
+
+                // If we have a valid copy statement we create it. Otherwise, we skip this and add the conditional as is.
+                if (isValidCopy) {
+                    optimizedStmts.emplace_back(new Transfer(stmt->column, stmt->line, addOffset, subOffset));
+
+                    if (offset > 0) {
+                        optimizedStmts.emplace_back(new IncrementPtr(stmt->column, stmt->line, offset));
+                    } else if (offset < 0) {
+                        optimizedStmts.emplace_back(new DecrementPtr(stmt->column, stmt->line, offset));
+                    }
+
+                    continue;
+                }
+            }
+
+            // If we cannot detect a reset statement, we simply add it untouched.
+            optimizedStmts.push_back(stmt);
         }
 
         return optimizedStmts;
--- src/Optimizer.h
+++ src/Optimizer.h
@@ -4,33 +4,73 @@
 
 #ifndef OPTIMIZER_H
 #define OPTIMIZER_H
+
 #include <vector>
 
+#include "Optimizer.h"
 #include "Payload.h"
 #include "Pipeline.h"
 #include "Stmt.h"
 
 namespace goo {
+    class OptimizationPass;
+
+    typedef std::vector<std::shared_ptr<Stmt>> StmtVector;
+    typedef std::vector<std::shared_ptr<OptimizationPass>> OptimizationVector;
+
     /// A class that analyzes a list of statements and tries to group similar statements in an attempt to reduce the
     /// number of operations and thus optimizing the code.
-    class Optimizer : public Phase {
-        std::vector<std::shared_ptr<Stmt>> optimizedStmts;
-
+    class Optimizer final : public Phase {
     public:
         explicit Optimizer(Reporter &reporter) : Phase(reporter) {
         }
 
         /// Analyses a list of statements and creates an optimized list that groups duplicates, as well as (in the
         /// future) unreachable or redundant code.
-        /// @param stmts A list of statements that are to be optimized. Nullptr-entries are being ignored.
-        /// @return A list of optimized statements.
+        /// @param payload A list of statements that are to be optimized. Nullptr-entries are being ignored.
+        /// @return A statement payload containing a list of optimized statements.
         std::shared_ptr<Payload> run(std::shared_ptr<Payload> payload) override;
 
-    private:
-        std::vector<std::shared_ptr<Stmt>> optimizeStmts(const std::vector<std::shared_ptr<Stmt>> &stmts);
-
-        std::vector<std::shared_ptr<Stmt>> groupStmts(int &idx, const std::vector<std::shared_ptr<Stmt>> &stmts);
     };
+
+    /// An optimization pass that transforms a list of statements.
+    class OptimizationPass {
+    public:
+        virtual ~OptimizationPass() = default;
+
+        [[nodiscard]] virtual StmtVector run(const StmtVector &stmts) const = 0;
+    };
+
+    /// An optimization pass that matches patterns of repeating operations of type byte increase/decrease and
+    /// pointer increase/decrease.
+    class GroupPass final : public OptimizationPass {
+    public:
+        [[nodiscard]] StmtVector run(const StmtVector &stmts) const override;
+
+    private:
+        /// Groups a list of statements of a similar type (*BYTE or *PTR) into one single statement. The idx-param
+        /// points to the first statement to group. After the execution of the method, the index points to the last
+        /// element that was grouped, to allow for regular processing in the caller.
+        /// @param idx The current index of stmts where grouping should begin.
+        /// @param stmts A list of statements to group.
+        /// @return A statement that groups a list of successive statements.
+        std::shared_ptr<Stmt> groupStmts(int &idx, const StmtVector &stmts) const;
+    };
+
+    /// A pass that detect patterns of type [-] and replaces them with a reset statement. If the reset pattern is
+    /// immediately followed by an INC_BYTE ( [-]+ ), the count of the INC_BYTE is used as initial value, otherwise 0.
+    class ResetPass final : public OptimizationPass {
+    public:
+        [[nodiscard]] StmtVector run(const StmtVector &stmts) const override;
+    };
+
+    /// A pass that detect patterns of type [>+<-], [->+<], [<+>-] or [>-<+>] and replaces them with two reset statements which moves the
+    /// value of the copier to the copy and clear the copies.
+    class TransferPass final : public OptimizationPass {
+    public:
+        [[nodiscard]] StmtVector run(const StmtVector &stmts) const override;
+    };
+
 }
 
 #endif //OPTIMIZER_H
